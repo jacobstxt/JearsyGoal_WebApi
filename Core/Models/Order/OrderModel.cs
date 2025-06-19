5a0aeed3c7b4d88ec917a6eac03e6865deccd8c6
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-06-19 15:34:59 +0300

    View Order is working now)

---
+++ Core/Models/Order/OrderModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.Order
+{
+    public class OrderModel
+    {
+        public long Id { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public DateTime DateCreated { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<OrderItemModel>? OrderItems { get; set; }
+    }
+}
---
+++ Core/Services/OrderService.cs
@@ -0,0 +1,34 @@
+﻿
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Core.Interfaces;
+using Core.Models.Order;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Services
+{
+    public class OrderService(IAuthService authService, AppDbJerseyGoalContext context, IMapper mapper) : IOrderService
+    {
+        public async Task<List<OrderModel>> GetOrdersAsync()
+        {
+            var userId = await authService.GetUserId();
+
+            var orderModelList = await context.Orders
+                .Where(x => x.UserId == userId)
+                .ProjectTo<OrderModel>(mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            orderModelList = orderModelList
+            .Select(item =>
+            {
+                item.TotalPrice = item.OrderItems.Sum(oi => oi.PriceBuy * oi.Count);
+                return item;
+            })
+            .ToList();
+
+            return orderModelList;
+        }
+
+    }
+}
